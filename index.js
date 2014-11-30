8b8260a69f90500411289aee835776274b2bd4c7
Tochukwu Okoro <tcokoro@gmail.com>
2014-11-29 17:13:40 -0800

    refactored css

--- index.js
+++ index.js
@@ -3,30 +3,14 @@ $(document).ready(function(){
 	// Change the header styling when the user scrolls
 	$(window).scroll(function(e){
 		if ($(window).scrollTop() < 20 ){
-			if (fixedHeader){
-				$(".cover-pg-header")
-					.css('background', 'transparent')
-					.css('color', 'white')
-					.css('border', '0')
-				$("header .early-access-btn")
-					.css('background', 'transparent')
-					.css('color', 'white')
-					.css('border', '2px solid white')
-				fixedHeader = false;
-			}
-
-		} else {
-			$(".cover-pg-header")
-				.css('background-color', 'white')
-				.css('color', 'black')
-				.css('border-bottom', '2px solid rgb(230, 230, 230)')
-			$("header .early-access-btn")
-				.css('background-color', 'rgb(219,68,55)')
-				.css('border', '0')
-
-			fixedHeader = true;
+			$("header").removeClass('scrolled-header');
+		} else {	
+			$("header").addClass('scrolled-header');
 		}
-		console.log("scroll from top: ", $(window).scrollTop());
+		
+		// Handle Parallax Scrolling
+		console.log('general info: ', $(".general-info-pg").scrollTop());
+		console.log('available locations: ', $(".available-locations-container").scrollTop());
 	});
 
 });
